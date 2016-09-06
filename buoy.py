a44ed638d7aa1411c51221e58d61757b4202a5a4
PrafulShankar <prflshankar@gmail.com>
2016-09-06 14:25:58 +0530

    added a control for buoy

--- buoy.py
+++ buoy.py
@@ -119,5 +119,5 @@ for each in circles_dict:
 					  +' r: '+str(i[2]), (i[0]-50,i[1]+40), cv2.FONT_HERSHEY_SIMPLEX, 0.4, \
 					  (0,0,0),2)
 cv2.imshow("targets", img)
-cv2.imwrite("target-buoys.jpg",img)
+cv2.imwrite("target-"+args["image"],img)
 cv2.waitKey(0)
